120a5a586964018599cf9394e08cae58f9abc6e2
Ratibor Živanović <zivanovicratibor@gmail.com>
2022-11-06 03:02:53 +0100

    Pure JWT

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,78 +2,71 @@ import { NextApiHandler } from 'next';
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 // import GitHubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google'
+// import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import prisma from '../../../lib/prisma';
 import { signIn, signOut } from 'next-auth/react';
 
 const options = {
     providers: [
-      // GitHubProvider({
-      //   clientId: process.env.GITHUB_ID,
-      //   clientSecret: process.env.GITHUB_SECRET,
-      // }),
-      GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID as string,
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-        })
+        // NOTE: https://www.youtube.com/watch?v=ollnut-J47s
+        CredentialsProvider({
+            type: "credentials",
+            credentials: {},
+            async authorize(credentials, req) {
+              const { email, password } = credentials as {
+                email: string;
+                password: string;
+              };
+              // perform you login logic
+              // find out user from db
+            //   if (email !== "john@gmail.com" || password !== "1234") {
+            //     throw new Error("invalid credentials");
+            //   }
+
+            const user = await prisma.user?.findUnique({
+                //   where: {email: 'test@gmail.com'}
+                where: {
+                    email: 'zivanovicratibor@gmail.com',
+                },
+            });
+      
+              // if everything is fine
+            //   return {
+            //     id: "1234",
+            //     name: "John Doe",
+            //     email: "john@gmail.com",
+            //     role: "admin",
+            //   };
+            user.role = 'admin';
+            return user;
+            },
+          }),
     ],
     // adapter: PrismaAdapter(prisma),
     secret: process.env.JWT_SECRET,
     callbacks: {
-      // https://next-auth.js.org/configuration/callbacks
-      async signIn({account, profile}: {account: Object, profile: Object}){
-          // console.log('ACCOUNT ', account);
-          // console.log('PROFILE ', profile);
-        //   return true;
-            // const user = await prisma.user?.findUnique({
-            //     //   where: {email: 'test@gmail.com'}
-            //     where: {
-            //         email: profile.email,
-            //     },
-            // });
-            // if(!user){ // This is potential new user
-            //     console.log(user);
-            //     return true;
-            // }
-            // else{
-            //     if(user.active){// User exist and it's active
-            //         return true;
-            //     }
-            //     console.log('User isn\'t active!');
-            //     return false;
-            //     // return '../../auth/login.tsx'
-            // }
-            const user = await prisma.user?.findUnique({
-                //   where: {email: 'test@gmail.com'}
-                where: {
-                    email: profile.email,
-                },
-            });
-            if(user?.active){
-                console.log("Valid user", user.email);
-                return true;
-            }
-            console.log('Invalid user');
-            return false;
-      },
-    //   async redirect({ url, baseUrl }: {url:string, baseUrl:string}) {
-    //       // Allows relative callback URLs
-    //       console.log(`${baseUrl}${url}`);
-    //       if (url.startsWith("/")) return `${baseUrl}${url}`
-    //       // Allows callback URLs on the same origin
-    //       else if (new URL(url).origin === baseUrl) return url
-    //       return baseUrl
-    //     }
         // NOTE: This will work only if you work with JWT. So without persisting session in database
         // https://next-auth.js.org/configuration/callbacks#jwt-callback
         async jwt({ token, user, account, profile, isNewUser }) {
-            console.log('JWT callback ', user);
+            console.log('JWT ');
+
+            if(user){
+                token.role = user.role;
+            }
             return token
-        }
+        },
+        session: async ({ session, token }) => {
+            console.log('SESSION ');
+            session.user.role = token.role;
+    
+            return session;
+            // return Promise.resolve(session);
+        },
     },
     pages:{
       // https://next-auth.js.org/configuration/pages
-      // signIn: '/auth/login',
+      signIn: '/auth/login',
       error: '/auth/error',
       newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
     }
--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,11 +1,47 @@
-import type { NextPage } from 'next'
-type Props = {
-    role: []
-  }
+import { NextPage } from "next";
+import { signIn } from "next-auth/react";
+import { FormEventHandler, useState } from "react";
 
-const Login: NextPage<Props> = (props) => {
+interface Props {}
 
-    return <h1>Login!</h1>
-}
+const SignIn: NextPage = (props): JSX.Element => {
+  const [userInfo, setUserInfo] = useState({ email: "", password: "" });
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    // validate your userinfo
+    e.preventDefault();
 
-export default Login
+    const res = await signIn("credentials", {
+      email: userInfo.email,
+      password: userInfo.password,
+      redirect: false,
+    });
+
+    console.log(res);
+  };
+  return (
+    <div className="sign-in-form">
+      <form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <input
+          value={userInfo.email}
+          onChange={({ target }) =>
+            setUserInfo({ ...userInfo, email: target.value })
+          }
+          type="email"
+          placeholder="john@email.com"
+        />
+        <input
+          value={userInfo.password}
+          onChange={({ target }) =>
+            setUserInfo({ ...userInfo, password: target.value })
+          }
+          type="password"
+          placeholder="********"
+        />
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
